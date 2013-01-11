e0550c521ee6c7a644ad7aeec0fda8a4c921431e
Dave Bertrand <dbertrand@imagescape.com>
2013-01-11 23:02:20 +0000

    removed custom tinymce work

--- newscenter/forms.py
+++ newscenter/forms.py
@@ -3,13 +3,10 @@ from django.conf import settings
 from django.db.models import get_model
 from newscenter.widgets import SmallTextField
 
-if 'tinymce' in settings.INSTALLED_APPS:
-    from tinymce.widgets import TinyMCE
-else:
-    from newscenter.widgets import TinyMCE
-
 class ArticleAdminModelForm(forms.ModelForm):
-    body = forms.CharField(widget=TinyMCE())
+    if 'tinymce' in settings.INSTALLED_APPS:
+        from tinymce.widgets import TinyMCE
+        body = forms.CharField(widget=TinyMCE())
     teaser = forms.CharField(required=False, widget=SmallTextField())
 
     class Meta:
