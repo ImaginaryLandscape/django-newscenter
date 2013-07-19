93279a57e58105fd638523272ecfed2633e85e53
David Bertrand <dbertrand@imagescape.com>
2013-07-19 10:43:50 -0500

    Switched to a try except for the url import for backwards compatibility

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='newscenter',
-    version='1.1.9',
+    version='1.1.10',
     description='A News Release Application for Django',
     long_description=open('README.rst').read(),
 
