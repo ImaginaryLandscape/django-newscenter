ea72016e4212d5402257135ff98c51af00c4f72d
David Bertrand <dbertrand@imagescape.com>
2014-08-19 14:24:25 +0000

    version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='newscenter',
-    version='1.3',
+    version='1.3.1',
     description='A News Release Application for Django',
     long_description=open('README.rst').read(),
 
