33f1a5165c3cc5663759b6b4e0887314c559158f
David Bertrand <dbertrand@imagescape.com>
2014-11-26 16:44:53 +0000

    Support for django light draft in article admin

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='newscenter',
-    version='1.4.5',
+    version='1.5',
     description='A News Release Application for Django',
     long_description=open('README.rst').read(),
 
