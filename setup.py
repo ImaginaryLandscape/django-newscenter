f5bfb76dacd0ed1057762435bbb522d107029ab0
David Bertrand <dbertrand@imagescape.com>
2015-07-08 16:46:52 +0000

    Upped minor version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='newscenter',
-    version='1.5.8',
+    version='1.6.0',
     description='A News Release Application for Django',
     long_description=open('README.rst').read(),
 
