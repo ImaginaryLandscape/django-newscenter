6638b4169f0cdf224ee54a06bef31212f27b2434
David Bertrand <dbertrand@imagescape.com>
2017-08-18 21:43:52 +0000

    Added config setting to exclude certain newsrooms

--- newscenter/views.py
+++ newscenter/views.py
@@ -262,6 +262,7 @@ class RandomFeatured(RedirectView):
     permanent = False
 
     def get_redirect_url(self, *args, **kwargs):
-        featured = models.Article.objects.get_featured()
+        exclude_newsrooms = getattr(settings, 'NEWSCENTER_EXCLUDE_RANDOM_FEATURED_NEWSROOMS', [])
+        featured = models.Article.objects.get_featured().exclude(newsroom__slug__in=exclude_newsrooms)
         article = random.choice(featured)
         return article.get_absolute_url()
