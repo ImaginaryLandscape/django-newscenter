cb78af37c03ef0333c2350954960951ec97cc521
David Bertrand <dbertrand@imagescape.com>
2018-11-16 11:21:02 -0600

    Added minlength var

--- newscenter/forms.py
+++ newscenter/forms.py
@@ -8,7 +8,8 @@ from django.conf import settings
 from newscenter.widgets import SmallTextField
 
 REQUIRE_TEASER = getattr(settings, 'NEWSCENTER_REQUIRE_TEASER', False)
-TITLE_MAXLENGTH = getattr(settings, 'NEWSCENTER_TITLE_MAXLENGTH', '')
+TITLE_MAXLENGTH = getattr(settings, 'NEWSCENTER_TITLE_MAXLENGTH', 400)
+TITLE_MINLENGTH = getattr(settings, 'NEWSCENTER_TITLE_MINLENGTH', 0)
 
 class ArticleAdminModelForm(forms.ModelForm):
     if 'djangocms_text_ckeditor' in settings.INSTALLED_APPS:
@@ -18,8 +19,9 @@ class ArticleAdminModelForm(forms.ModelForm):
         from tinymce.widgets import TinyMCE
         body = forms.CharField(widget=TinyMCE())
     teaser = forms.CharField(required=REQUIRE_TEASER, widget=SmallTextField())
-    title = forms.CharField(widget=forms.TextInput(
-        attrs={'maxlength': TITLE_MAXLENGTH}))
+    title = forms.CharField(max_length=TITLE_MAXLENGTH, 
+        min_length=TITLE_MINLENGTH,
+        widget=forms.TextInput(attrs={'maxlength': TITLE_MAXLENGTH}))
 
     class Meta:
         model = get_model('newscenter', 'article')
