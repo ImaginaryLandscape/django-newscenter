44e53a30465f2fea4e6f6de7490f80ca0310d067
David Bertrand <dbertrand@imagescape.com>
2019-02-22 20:42:20 +0000

    Updated cms apphook

--- cmsplugin_newscenter/cms_apps.py
+++ cmsplugin_newscenter/cms_apps.py
@@ -2,9 +2,11 @@ from cms.app_base import CMSApp
 from cms.apphook_pool import apphook_pool
 from django.utils.translation import ugettext_lazy as _
 
-
-class NewsCenter(CMSApp):
+class Newscenter(CMSApp):
+    app_name = "newscenter"
     name = _("News Center")
-    urls = ["newscenter.urls"]
+    
+    def get_urls(self, page=None, language=None, **kwargs):
+        return ["newscenter.urls"]
 
 apphook_pool.register(NewsCenter)
