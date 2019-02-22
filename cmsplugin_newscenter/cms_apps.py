2ddf90a3949c6e46e072997272a73f6dbf305372
David Bertrand <dbertrand@imagescape.com>
2019-02-22 21:05:35 +0000

    Fixed apphook name

--- cmsplugin_newscenter/cms_apps.py
+++ cmsplugin_newscenter/cms_apps.py
@@ -2,7 +2,7 @@ from cms.app_base import CMSApp
 from cms.apphook_pool import apphook_pool
 from django.utils.translation import ugettext_lazy as _
 
-class Newscenter(CMSApp):
+class NewsCenter(CMSApp):
     app_name = "newscenter"
     name = _("News Center")
     
