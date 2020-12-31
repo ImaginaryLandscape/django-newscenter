0372d61307f93cd16e84dcf04dc0ba365c562c42
David Bertrand <dbertrand@imagescape.com>
2020-12-31 10:53:58 -0600

    on_delete attrs in plugin migration

--- cmsplugin_newscenter/migrations/0001_initial.py
+++ cmsplugin_newscenter/migrations/0001_initial.py
@@ -14,9 +14,9 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='NewsFeedPluginModel',
             fields=[
-                ('cmsplugin_ptr', models.OneToOneField(parent_link=True, related_name='cmsplugin_newscenter_newsfeedpluginmodel', auto_created=True, primary_key=True, serialize=False, to='cms.CMSPlugin')),
+                ('cmsplugin_ptr', models.OneToOneField(parent_link=True, related_name='cmsplugin_newscenter_newsfeedpluginmodel', auto_created=True, primary_key=True, serialize=False, to='cms.CMSPlugin', on_delete=models.CASCADE)),
                 ('limit', models.IntegerField(default=5, help_text=b'Maximum number of articles to display', verbose_name=b'Article Limit')),
-                ('location', models.ForeignKey(to='newscenter.Feed')),
+                ('location', models.ForeignKey(to='newscenter.Feed', on_delete=models.CASCADE)),
             ],
             options={
                 'db_table': 'newscenter_newsfeedpluginmodel',
