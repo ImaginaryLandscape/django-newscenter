757abc1599909ab6c2d9bf63cc96d6e51aa88173
David Bertrand <dbertrand@imagescape.com>
2021-01-04 09:43:07 -0600

    updated reverse import in cms plugin

--- cmsplugin_newscenter/cms_toolbars.py
+++ cmsplugin_newscenter/cms_toolbars.py
@@ -1,4 +1,7 @@
-from django.core.urlresolvers import reverse
+try:
+    from django.urls import reverse
+except:
+    from django.core.urlresolvers import reverse
 from django.utils.translation import ugettext_lazy as _
 from cms.toolbar_base import CMSToolbar
 from cms.toolbar_pool import toolbar_pool
