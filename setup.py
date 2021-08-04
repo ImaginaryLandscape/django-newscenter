1977d2c8ad8f1fb402ac449552cd0c7619c9385a
David Bertrand <dbertrand@imagescape.com>
2021-08-04 16:47:34 -0500

    Fixed plugin url name in template

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='newscenter',
-    version='3.0.1',
+    version='3.0.2',
     description='A News Release Application for Django',
     long_description=open('README.rst').read(),
 
