94a3908c959c9edaa74b677709106ec0ee3cfadc
Sergey Korolevsky <sergeykorolevsky2015@gmail.com>
2023-07-05 12:52:19 +0300

    update urls

--- newscenter/urls.py
+++ newscenter/urls.py
@@ -1,8 +1,8 @@
 from django.views.generic.list import ListView
 try:
-    from django.conf.urls import url
+    from django.urls import re_path
 except ImportError:
-    from django.conf.urls.defaults import url
+    from django.conf.urls import include
 from .views import (
     NewsroomIndex, NewsroomLatest, NewsroomRandom, ArticleDetail,
     ArchiveYear, ArchiveMonth, newsroom_detail, category_detail)
@@ -11,7 +11,7 @@ from . import models
 
 # ## Object List
 urlpatterns = [
-    url(r'^categories/$', ListView.as_view(
+    re_path(r'^categories/$', ListView.as_view(
         queryset=models.Category.objects.all(),
         allow_empty=True,
     )),
@@ -19,24 +19,24 @@ urlpatterns = [
 
 # ## Custom
 urlpatterns += [
-    url(r'^$', NewsroomIndex.as_view(), name="news_newsroom_index"),
-    url(r'^(?P<slug>[\-\d\w]+)/$', newsroom_detail,
+    re_path(r'^$', NewsroomIndex.as_view(), name="news_newsroom_index"),
+    re_path(r'^(?P<slug>[\-\d\w]+)/$', newsroom_detail,
         name='news_newsroom_detail'),
-    url(r'^categories/(?P<slug>[\-\d\w]+)/$', category_detail,
+    re_path(r'^categories/(?P<slug>[\-\d\w]+)/$', category_detail,
         name='news_category_detail'),
-    url(r'^(?P<newsroom>[\-\d\w]+)/(?P<year>\d{4})/(?P<month>[a-z]{3})/(?P<slug>[\-\d\w]+)/$',
+    re_path(r'^(?P<newsroom>[\-\d\w]+)/(?P<year>\d{4})/(?P<month>[a-z]{3})/(?P<slug>[\-\d\w]+)/$',
         ArticleDetail.as_view(), name='news_article_detail'),
-    url(r'^(?P<newsroom>[\-\d\w]+)/(?P<year>\d{4})/(?P<month>[a-z]{3})/$',
+    re_path(r'^(?P<newsroom>[\-\d\w]+)/(?P<year>\d{4})/(?P<month>[a-z]{3})/$',
         ArchiveMonth.as_view(), name='news_archive_month',),
-    url(r'^(?P<newsroom>[\-\d\w]+)/(?P<year>\d{4})/$',
+    re_path(r'^(?P<newsroom>[\-\d\w]+)/(?P<year>\d{4})/$',
         ArchiveYear.as_view(), name='news_archive_year',),
-    url(r'^(?P<newsroom>[\-\d\w]+)/latest/$',
+    re_path(r'^(?P<newsroom>[\-\d\w]+)/latest/$',
         NewsroomLatest.as_view(), name='news_newsroom_latest'),
-    url(r'^(?P<newsroom>[\-\d\w]+)/random/$',
+    re_path(r'^(?P<newsroom>[\-\d\w]+)/random/$',
         NewsroomRandom.as_view(), name='news_newsroom_random')
 ]
 
 # ## Feeds
 urlpatterns += [
-    url(r'^(?P<newsroom>[\-\d\w]+)/rss/$', NewsroomFeed(), {}, name='newsroom_feed'),
+    re_path(r'^(?P<newsroom>[\-\d\w]+)/rss/$', NewsroomFeed(), {}, name='newsroom_feed'),
 ]
