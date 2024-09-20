9f06569763b7e9d3c724ef7c66221982f74a4ff0
Noel Taylor <ntaylor@imagescape.com>
2024-09-20 18:21:42 +0000

    Use a 'ckeditor' widget for the 'body' field if 'ckeditor' is available but other options aren't; bump version

--- newscenter/forms.py
+++ newscenter/forms.py
@@ -14,10 +14,13 @@ TITLE_MINLENGTH = getattr(settings, 'NEWSCENTER_TITLE_MINLENGTH', 0)
 class ArticleAdminModelForm(forms.ModelForm):
     if 'djangocms_text_ckeditor' in settings.INSTALLED_APPS:
         from djangocms_text_ckeditor.widgets import TextEditorWidget
-        body = forms.CharField(required=False, widget=TextEditorWidget())
+        body = forms.CharField(widget=TextEditorWidget())
     elif 'tinymce' in settings.INSTALLED_APPS:
         from tinymce.widgets import TinyMCE
         body = forms.CharField(widget=TinyMCE())
+    elif 'ckeditor' in settings.INSTALLED_APPS:
+        from ckeditor.widgets import CKEditorWidget
+        body = forms.CharField(widget=CKEditorWidget())
     teaser = forms.CharField(required=REQUIRE_TEASER, widget=SmallTextField())
     title = forms.CharField(max_length=TITLE_MAXLENGTH,
         min_length=TITLE_MINLENGTH,
