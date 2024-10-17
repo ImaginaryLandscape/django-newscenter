ee7309e9f06bcaa9f189a06fed90bb6f846886ef
Noel Taylor <ntaylor@imagescape.com>
2024-10-17 16:21:38 +0000

    Reorder paths so DayOfWeek not blocking NewroomFeed; reformat all paths for readability; bump version

--- newscenter/urls.py
+++ newscenter/urls.py
@@ -7,7 +7,6 @@ from .views import (
 from .feeds import NewsroomFeed
 from . import models
 
-# ## Object List
 urlpatterns = [
     re_path(r'^categories/$', ListView.as_view(
         queryset=models.Category.objects.all(),
@@ -15,32 +14,20 @@ urlpatterns = [
     )),
 ]
 
-# ## Custom
 urlpatterns += [
     re_path(r'^$', NewsroomIndex.as_view(), name="news_newsroom_index"),
-    re_path(r'^random_featured/$', RandomFeatured.as_view(), 
-        name='news_random_featured'),
-    re_path(r'^(?P<slug>[\-\d\w]+)/$', newsroom_detail,
-        name='news_newsroom_detail'),
-    re_path(r'^categories/(?P<slug>[\-\d\w]+)/$', category_detail,
-        name='news_category_detail'),
-    re_path(r'^(?P<newsroom>[\-\d\w]+)/(?P<year>\d{4})/(?P<month>[a-z]{3})/(?P<slug>[\-\d\w]+)/$',
-        ArticleDetail.as_view(), name='news_article_detail'),
-    re_path(r'^(?P<newsroom>[\-\d\w]+)/(?P<year>\d{4})/(?P<month>[a-z]{3})/$',
-        ArchiveMonth.as_view(), name='news_archive_month',),
-    re_path(r'^(?P<newsroom>[\-\d\w]+)/(?P<year>\d{4})/$',
-        ArchiveYear.as_view(), name='news_archive_year',),
-    re_path(r'^(?P<newsroom>[\-\d\w]+)/(?P<dayofweek>[a-z]{3})/$',
-        DayOfWeek.as_view(), name='news_dayofweek',),
-    re_path(r'^d/(?P<slug1>[\-\d\w]+)_(?P<slug2>[\-\d\w]+)/$',
-        dual_newsrooms, name='news_dual_newsroons'),
-    re_path(r'^(?P<newsroom>[\-\d\w]+)/latest/$', 
-        NewsroomLatest.as_view(), name='news_newsroom_latest'),
-    re_path(r'^(?P<newsroom>[\-\d\w]+)/random/$',
-        NewsroomRandom.as_view(), name='news_newsroom_random')
+    re_path(r'^random_featured/$', RandomFeatured.as_view(), name='news_random_featured'),
+    re_path(r'^(?P<slug>[\-\d\w]+)/$', newsroom_detail, name='news_newsroom_detail'),
+    re_path(r'^categories/(?P<slug>[\-\d\w]+)/$', category_detail, name='news_category_detail'),
+    re_path(r'^(?P<newsroom>[\-\d\w]+)/rss/$', NewsroomFeed(), {}, name='newsroom_feed'),
+    re_path(r'^(?P<newsroom>[\-\d\w]+)/(?P<year>\d{4})/(?P<month>[a-z]{3})/(?P<slug>[\-\d\w]+)/$', ArticleDetail.as_view(), name='news_article_detail'),
+    re_path(r'^(?P<newsroom>[\-\d\w]+)/(?P<year>\d{4})/(?P<month>[a-z]{3})/$', ArchiveMonth.as_view(), name='news_archive_month',),
+    re_path(r'^(?P<newsroom>[\-\d\w]+)/(?P<year>\d{4})/$', ArchiveYear.as_view(), name='news_archive_year',),
+    re_path(r'^(?P<newsroom>[\-\d\w]+)/(?P<dayofweek>[a-z]{3})/$', DayOfWeek.as_view(), name='news_dayofweek',),
+    re_path(r'^d/(?P<slug1>[\-\d\w]+)_(?P<slug2>[\-\d\w]+)/$', dual_newsrooms, name='news_dual_newsroons'),
+    re_path(r'^(?P<newsroom>[\-\d\w]+)/latest/$', NewsroomLatest.as_view(), name='news_newsroom_latest'),
+    re_path(r'^(?P<newsroom>[\-\d\w]+)/random/$', NewsroomRandom.as_view(), name='news_newsroom_random')
 ]
 
-# ## Feeds
 urlpatterns += [
-    re_path(r'^(?P<newsroom>[\-\d\w]+)/rss/$', NewsroomFeed(), {}, name='newsroom_feed'),
 ]
